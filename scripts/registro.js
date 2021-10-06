19e30215c0c59dc2678f79433f9e9a96b8212d73
DavidIsazaArias <david.isaza@trebol-it.cl>
2021-10-05 21:46:28 -0500

    ADD functions agregarregistro and filtrarPorContrasena

--- scripts/registro.js
+++ scripts/registro.js
@@ -1,44 +1,50 @@
-var registros = [
-    { usuario: "david",
-      con: "123.43" },
-    { usuario: "Diana",
-      con: "1234567" }];
+let registros = [{username: "David", password: "1234567"}];
+      
+
+let form = document.getElementById("btn-submit-form");
+if (form){
+    form.addEventListener("submit", agregarRegistro());
+}
+
+function credentials(username, password) {
+    this.username = username;
+    this.password = password;
+}
 
 
 function agregarRegistro(){
-    let user = document.findElementById("in_usuario").value;
-    let password = document.findElementById("in_contrasena").value;
-    let newUser = {usuario: user,con: password};
+    let user = document.getElementById("in_usuario").value;
+    let password = document.getElementById("in_contrasena").value;
+    console.log(user);
+    console.log(password);
+    let credencial = new credentials(user,password);
     console.log("test")
-    registros.push(newUser);
     if(validar_nombreUsuario(user) && validar_contrasena(password)){
-       
+       registros.push(credencial); 
+       console.log(registros);
+    }  
+   }
+
+    function filtrarPorContrasena(datos, filtro){
+        return datos.filter(function(credential){
+            return credential.con.length <= filtro 
+        })
     }
-}
-function filtrarPorContrasena(datos, filtro){
-    return datos.filter(function(credential){
-        return credential.con.length <= filtro 
-    })
-}
-function validar_nombreUsuario(string) {
-    const expresion=new RegExp('^[A-Z0-9Ñ]+$','i');
-    return string ? string.length > 3 && string.length <= 9? expresion.test(string):false:false;
-    //console.log("Validacion nombre: ",result);
-    //return result;
-}
-function validar_contrasena(string) {
-    const expresion=new RegExp('^[A-Z0-9Ñ]+$','i');
-    return string.length >= 6 ? expresion.test(string) : false
-    //console.log("validacion contraseña: ", result)
-}
+    function validar_nombreUsuario(string) {
+        const expresion=new RegExp('^[A-Z0-9Ñ]+$','i');
+        let result = string ? string.length > 3 && string.length <= 9? expresion.test(string):false:false;
+        console.log("Validacion nombre: ",result);
+         
+        
+        //return result;
+    }
+    function validar_contrasena(string) {
+        const expresion=new RegExp('^[A-Z0-9Ñ]+$','i');
+        let result= string? string.length >= 6 ? expresion.test(string) : false:false;
+        console.log("validacion contraseña: ", result)
+            
+    }
+    
 
-console.log(registros);
-
-/*
-console.log("Atributos 1 objeto 1: ", registros[0].user)
-
-let results = filtrarPorContrasena(registros,4)
-console.log("Resultado 1: ", results)
-let results2 = filtrarPorContrasena(registros,6)
-console.log("Resultado 2: ", results2)*/
-module.exports.agregarRegistro();
+    console.log(registros);
+exports.modules = {registros,agregarRegistro,filtrarPorContrasena}
